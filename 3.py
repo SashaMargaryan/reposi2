1756304a165cf3464ec188e12c4f808d55cdb787
SashaMargaryan <53489553+SashaMargaryan@users.noreply.github.com>
2019-08-02 21:11:29 +0400

    Update 3.py

--- 3.py
+++ 3.py
@@ -2,4 +2,4 @@ a=int(input("mtqagrel arajin tic@:a"))
 b=int(input("mtqagrel erkrord tiv@:b"))
 c=int(input("mutqagrel erod tiv@:c"))
 
-print("hasvel mijin tvabanakan@:",int((10+20+30)/3))
+print("hasvel mijin tvabanakan@:",int((a+b+c)/3))
