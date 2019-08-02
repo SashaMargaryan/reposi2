a0632b70c26a663f001746913a97ce10ed45d075
SashaMargaryan <53489553+SashaMargaryan@users.noreply.github.com>
2019-08-02 23:21:30 +0400

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,9 +1,7 @@
 a=int(input("mtqagrel arajin andami arjeQ@:a1"))
 d=int(input("mutqagrel tarberutyun@:d"))
 n=int(input("mutqagel n-rd andami hamar@:n"))
-a=5
-d=10
-n=9
+
 
 print("aN-@ klini:",int(a+(n-1)+d))
 
