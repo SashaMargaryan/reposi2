263e317d22cc6bb0ed5f9bf832a945d917aea4b2
SashaMargaryan <53489553+SashaMargaryan@users.noreply.github.com>
2019-08-02 21:15:16 +0400

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,4 +1,4 @@
-a1=int(input("mtqagrel arajin andami arjeQ@:a1"))
+a=int(input("mtqagrel arajin andami arjeQ@:a1"))
 d=int(input("mutqagrel tarberutyun@:d"))
 n=int(input("mutqagel n-rd andami hamar@:n"))
 a=5
